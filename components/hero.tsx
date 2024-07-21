6239e264d523f657a03bedb739f1373f338cf273
Sina <sina.meraji@gmail.com>
2024-07-21 16:12:53 -0700

    edit title and desc

--- components/hero.tsx
+++ components/hero.tsx
@@ -126,7 +126,7 @@ export default function Hero() {
                   href="#"
                   className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
                 >
-                  Try
+                  Sign up
                 </a>
                 
               </div>
